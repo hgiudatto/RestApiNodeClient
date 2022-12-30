b6beac5cba5d666d86bb46dde02a1c4df73d5e67
Your Name <hector.giudatto@gmail.com>
2022-12-30 18:16:57 -0300

    Rick and Morty Promise.all

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-const Client = require("node-rest-client").Client;
+import pkg, { getRickAndMortyCharactersByIds } from "./server.cjs";
+const { getRickAndMortyCharacters, getRickAndMortyCharactersById } = pkg;
 
+// * https://rickandmortyapi.com/documentation
+
+/*
+ **
+ * node-rest-client
+ **
 let client = new Client();
-
 client.get("https://rickandmortyapi.com/api/character/2", (data, response) => {
   console.log(
     `Name: ${data.name} - Species: ${data.species} - Gender: ${data.gender} - Status: ${data.status} - Image: ${data.image}`
   );
 });
+ */
 
-// TODO: https://rickandmortyapi.com/documentation
-// TODO: Testear GET con https://github.com/afuh/rick-and-morty-api-node#get-by-id
-// TODO: Continuar: https://medium.com/nerd-for-tech/your-first-nodejs-rest-api-client-59467659ab99
+let args = [8, 15, 19];
+
+// getRickAndMortyCharactersById(1);
+// getRickAndMortyCharactersByIds.apply(null, args);
+
+(() => {
+  console.log(`Promise.all:`);
+  Promise.all([
+    getRickAndMortyCharactersById(1),
+    getRickAndMortyCharactersByIds.apply(null, args),
+    getRickAndMortyCharacters(),
+  ]);
+})();
